57bc69f89af4717419dd5e3ec0ad8e222fc99987
Alex Dunn <alexander.dunn@outlook.com>
2017-06-19 09:57:16 -0400

    initial project set up from VS for Mac including .net core API backend

---
+++ SurveyApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SurveyApp
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
